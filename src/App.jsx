569beffda63d0079ddbc3a322bea30a90065a66b
mayanja <mayanjer3@gmail.com>
2025-12-29 09:53:21 +0300

    added the useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,42 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data";
+import { useState } from "react";
 
 function App() {
-  let tabContent = "Please click a button"
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
   function handleSelect(selectedButton) {
-    tabContent=selectedButton
-  }
-    
-   
-    return (
-      <div>
-        <Header />
-        <main>
-          <section id="core-concepts">
-            <ul>
-              <CoreConcepts {...CORE_CONCEPTS[0]} />
-              <CoreConcepts {...CORE_CONCEPTS[1]} />
-              <CoreConcepts {...CORE_CONCEPTS[2]} />
-              <CoreConcepts {...CORE_CONCEPTS[3]} />
-            </ul>
-          </section>
-          <section id="examples">
-            <h2>Examples</h2>
-            <menu>
-              <TabButton onSelect={() => handleSelect("components")}>
-                Components
-              </TabButton>
-              <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
-              <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-              <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-            </menu>
-            {tabContent}
-          </section>
-        </main>
-      </div>
-    );
+    setSelectedTopic(selectedButton);
   }
 
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {selectedTopic}
+        </section>
+      </main>
+    </div>
+  );
+}
 
 export default App;
-
-// element.addEventListener(()=>{
-//   console.log("Hello world")})
